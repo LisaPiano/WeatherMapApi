a74aa0cc87edaabb94bc66198d3ffba1e71f221e
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-12 22:32:52 -0600

    changed gitignore

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace WeatherMapApi
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using static System.Net.WebRequestMethods;
+
+namespace WeatherMapApi
 {
-    class Programn
+
+    class Program
     {
-        private static void Main(string[] args)
+
+        static void Main(string[] args)
         {
-           
-        }
-    }
-}
+            //var lat = 37.270500;
+            //var lon = -107.878700;
+            var api = "afca3ddb684ef8460ebde8a89727053f";
+            var city = "durango";
+
+            var client = new HttpClient();
+            var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api}&units=imperial";
+            var weatherResponse = client.GetAsync(weatherURL).Result;
+            Console.WriteLine(weatherResponse);
+            //JObject parsedResponse = JObject.Parse(weatherResponse);
+            //var temp = parsedResponse["list"][0]["main"]["temp"];
+            //Console.WriteLine(temp);
+
+
+    }//end main method
+  }//end class
+}//end namespace
